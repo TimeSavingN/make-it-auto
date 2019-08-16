3d11323b1e4e8fc08f26aac471ed8e7effc28925
taogen <taogenjia@gmail.com>
2019-08-16 16:24:28 +0800

    Update

--- develop-tools/open-jdk-1.8-install-from-repository-linux.sh
+++ develop-tools/open-jdk-1.8-install-from-repository-linux.sh
@@ -16,22 +16,50 @@ function get_package_manager
 	echo "$package_manager"
 }
 
+# Get Valid Answer From Terminal
+# return: $answer
+function get_valid_answer
+{
+	message="$1 (yes/no): "
+	echo -n "$message"
+	read answer
+	while true; do
+		if [ "$answer" = "yes" ] || [ "$answer" = "no" ]; then
+			break;
+		else
+			echo -n $message
+			read answer
+		fi
+	done
+}
+
+# Get number of range from Terminal Input
+# return $answer_num
+function get_number_of_range
+{
+	min=$2
+	max=$3
+    message="$1. Input ($min~$max): "
+	echo -n "$message"
+	read answer_num
+	while true; do
+		if [ "$answer_num" -ge "$min" ] && [ "$answer_num" -le "$max" ]; then
+			break;
+		else
+			echo -n $message
+			read answer_num
+		fi
+	done
+}
+
+
 
 # -------------------------------------
 # my functions
 # -------------------------------------
 
-
-# -------------------------------------
-# main
-# -------------------------------------
-
-# Check package manager
-package_manager=$(get_package_manager)
-echo -e "\n\n Package manager is $package_manager ! \n\n"
-
-# check is installed
-if ! [ -x "$(command -v java)" ]; then
+function install_jdk_default
+{
 	# yum
 	if [ "$package_manager" = "yum" ]; then
 		# 1. Search OpenJDK Packages
@@ -41,15 +69,6 @@ if ! [ -x "$(command -v java)" ]; then
 		echo -e "\n\n Install JDK 1.8 ... \n\n"
 		sudo yum install -y java-1.8.0-openjdk.x86_64
 		echo -e "\n\n Install JDK 1.8 is successful ! \n\n"
-		
-		# 3. Configure Default Java Version
-		java -version
-		
-		# 4. Set JAVA_HOME 
-		echo "JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.222.b10-0.el7_6.x86_64/" >> /etc/environment
-		source /etc/environment
-		echo -e "\n\n Your JAVA_HOME is $JAVA_HOME \n\n"
-		echo -e "\n\n install JDK 1.8 is successful ! \n\n"
 	fi
 
 	# apt
@@ -61,6 +80,41 @@ if ! [ -x "$(command -v java)" ]; then
 		echo -e "\n\n Install JDK 1.8 ... \n\n"
 		sudo apt-get install -y openjdk-8-jre openjdk-8-jdk
 		echo -e "\n\n Install JDK 1.8 is successful ! \n\n"
+	fi
+}
+
+function install_jdk_selected
+{
+	if [ "$package_manager" = "apt" ]; then
+		mapfile -t jdk_array < <( sudo apt-cache search openjdk | grep -E '^openjdk-[0-9]{1,2}-jdk' )
+		sudo apt-cache search openjdk | grep -E '^openjdk-[0-9]{1,2}-jdk' | cat -b
+	fi
+	if [ "$package_manager" = "yum" ]; then
+		mapfile -t jdk_array < <( sudo yum search java- | grep -E 'java.+openjdk\.' )
+		sudo yum search java- | grep -E 'java.+openjdk\.' | cat -b
+	fi
+	
+	arr_len="${#jdk_array[@]}"
+	get_number_of_range "Please select your install version." 1 $arr_len
+	sudo $package_manager install ${jdk_array[answer_num]}
+}
+
+function configure_jdk
+{
+	# yum
+	if [ "$package_manager" = "yum" ]; then
+		# 3. Configure Default Java Version
+		java -version
+		
+		# 4. Set JAVA_HOME 
+		echo "JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.222.b10-0.el7_6.x86_64/" >> /etc/environment
+		source /etc/environment
+		echo -e "\n\n Your JAVA_HOME is $JAVA_HOME \n\n"
+		echo -e "\n\n install JDK 1.8 is successful ! \n\n"
+	fi
+	
+	# apt
+	if [ "$package_manager" = "apt" ]; then
 		
 		# 3. Configure Default Java Version
 		java -version
@@ -71,6 +125,27 @@ if ! [ -x "$(command -v java)" ]; then
 		echo -e "\n\n Your JAVA_HOME is $JAVA_HOME \n\n"
 		echo -e "\n\n install JDK 1.8 is successful ! \n\n"
 	fi
+}
+
+# -------------------------------------
+# main
+# -------------------------------------
+
+# Check package manager
+package_manager=$(get_package_manager)
+echo -e "\n\n Package manager is $package_manager ! \n\n"
+
+# check is installed
+if ! [ -x "$(command -v java)" ]; then
+	question="Does install default java-1.8-x64?"
+	get_valid_answer "$question"
+	if [ "$answer" = "yes" ]; then
+		install_jdk_default
+	else
+		install_jdk_selected
+	fi
+	
+	configure_jdk
 else
 	echo -e "\n\n You already install java! \n\n"
 	java -version
